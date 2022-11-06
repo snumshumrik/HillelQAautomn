988ca24528cf4778f7298451b675faa2af31af1e
Olga <snumshumrik1983@gmail.com>
2022-11-06 19:47:34 +0200

    -HW6

---
+++ src/main/java/lesson6/objects/Animal.java
@@ -0,0 +1,39 @@
+package lesson6.objects;
+
+public class Animal {
+
+   private String eats;
+   private int noOflegs;
+    boolean vegatarian;
+
+    public Animal(String eats,int noOflegs, boolean vegatarian) {
+        this.eats = eats;
+        this.noOflegs = noOflegs;
+        this.vegatarian=vegatarian;
+        super();
+    }
+
+    public void setName(String eats) {
+
+        this.eats = eats;
+    }
+
+    public String getName() {
+
+        return eats;
+    }
+
+    public class Animal {
+
+        private String eats;
+
+        public void setName(String eats) {
+            this.eats = eats;
+        }
+
+        public String getName(){
+            return eats;
+
+        }
+    }
+}
